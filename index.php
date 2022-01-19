ad0777ed78b43cc695a456504df989fd45bdd4dc
Jáchym Tomášek <87898573+JachymTomasek@users.noreply.github.com>
2022-01-19 21:46:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <html>
-<head>
-<title>Contact Form</title>
-</head>
-<body>
-<form action="send.php" method="post">
-name:<input name="name"type="text" /></br>
-e-mail:<input name="email" type="text" /></br>
-web:<input name="web" type="text" /></br>
-text:<textarea name="text"></textarea></br>
-</br>
-<input type="submit" class="button" value="send" />
-</form>
-</body>
+  <head>
+    <title>Contact Form</title>
+  </head>
+  <body>
+    <form action="send.php" method="post">
+      name:<input name="name"type="text" /></br>
+      e-mail:<input name="email" type="text" /></br>
+      web:<input name="web" type="text" /></br>
+      text:<textarea name="text"></textarea></br>
+      </br>
+      <input type="submit" class="button" value="send" />
+    </form>
+  </body>
 </html>
